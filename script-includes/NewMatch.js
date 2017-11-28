8f78a5c4fa50578381707a7ccff67472fc798d95
Manoj <durvasula@yahoo.com>
2017-11-28 10:26:32 +0530

    Initial Commit

---
+++ script-includes/NewMatch.js
@@ -0,0 +1,31 @@
+var NewMatch = Class.create();
+gs.print("Let's fight");
+gs.print("Lest test again");
+gs.print("Let's fight..Manoj.....");
+
+gs.print("Let not fight any more.");
+
+NewMatch.prototype = {
+    initialize: function() {
+    },
+	testMethod : function(){
+		var gr = new GlideRecord('sys_user');
+		gr.addActiveQuery();
+		gr.query();
+		while(gr.next()){
+			var userId = gr.getValue('user_id');
+			var name = gr.getValue('name');
+			gs.print("UserDetails"+userId+":"+name);
+		}
+	},
+
+
+	testMethod2 : function() {
+		gs.print("UserDetails");
+		gs.print("UserDetails");
+	},
+	
+
+
+    type: 'NewMatch'
+};
